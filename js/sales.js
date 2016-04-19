62b6b026de4d046eb7018917f9c21252a2a603f4
tanyaweaver <tanya83@uw.edu>
2016-04-19 16:16:48 -0700

    hours are added to the header, sales added to each hour for each store, all stores are in array, for-loop calls function for the table display for the array

--- js/sales.js
+++ js/sales.js
@@ -17,7 +17,7 @@ function LocationSales (storeName, minCustomer, maxCustomer, avgSales) {
   };
   this.hourlySales = function() {
     this.hSales = [];
-    for (var i = 0; i < this.hCustomer.length; i++) {
+    for (var i = 0; i < this.hourlyCustomer().length; i++) {
       this.hSales.push(Math.floor(this.hCustomer[i] * this.avgSales));
     };
     return this.hSales;
@@ -31,48 +31,53 @@ function LocationSales (storeName, minCustomer, maxCustomer, avgSales) {
   };
 };
 
-function putSalesInTable(location) {
-//table header gets hours from hours[]
+//table header gets 'Location', hours from hours[], and 'Total'
+function putHoursInTable() {
   var appendHeader = document.getElementById('hours');
+  var tr = document.createElement('tr');
   var th = document.createElement('th');
   th.textContent = ('Location');
-  appendHeader.appendChild(th);
+  tr.appendChild(th);
+  appendHeader.appendChild(tr);
+//adding hours to the header
   for (var i = 0; i < hours.length; i++){
-    var td = document.createElement('td');
-    var tr = document.createElement('tr');
-    td.textContent = hours[i];
-    appendHeader.appendChild(td);
+    var th = document.createElement('th');
+    th.textContent = hours[i];
+    tr.appendChild(th);
   }
+
   th = document.createElement('th');
   th.textContent = ('Total');
-  appendHeader.appendChild(th);
+  tr.appendChild(th);
+}
+
 // table body gets location sales
+function putSalesInTable(location) {
   var appendRow = document.getElementById('store-info');
-  //var tr = document.createElement('tr');
+  var tr = document.createElement('tr');
   var th = document.createElement('th');
-  th.textContent = location.storeName;
-  appendRow.appendChild(th);
+  th.textContent = location.storeName; //addidng location name to the first column
+  tr.appendChild(th);
+  appendRow.appendChild(tr);//only appending to <tbody> here in this function
+
   for (var i = 0; i < location.hourlySales().length; i++){
-    var td = document.createElement ('td');
-    var tr = document.createElement('tr');
-    td.textContent = location.hourlySales()[i];
-    //tr.appendChild(td);
-    appendRow.appendChild(td);
+    td = document.createElement ('td');
+    td.textContent = location.hourlySales()[i]; //addidng hourly sales to the row
+    tr.appendChild(td);
   }
+
   td = document.createElement('td');
-  td.textContent = location.salesDailyTotals();
-  //tr.appendChild(td);
-  appendRow.appendChild(td);
+  td.textContent = location.salesDailyTotals(); //adding total sales for each location to the last column
+  tr.appendChild(td);
 };
 
-var pikePlace = new LocationSales ('Pike Place', 17, 88, 5.2);
-var seaTacAirport = new LocationSales ('Sea Tac Airport', 6, 24, 1.2);
-var southcenter = new LocationSales ('Southcenter', 11, 38, 1.9);
-var bellevue = new LocationSales ('Bellevue', 20, 48, 3.3);
-var alki = new LocationSales ('Alki', 3, 24, 2.6);
+allStores.push(new LocationSales ('Pike Place', 17, 88, 5.2));
+allStores.push(new LocationSales ('Sea Tac Airport', 6, 24, 1.2));
+allStores.push(new LocationSales ('Southcenter', 11, 38, 1.9));
+allStores.push(new LocationSales ('Bellevue', 20, 48, 3.3));
+allStores.push(new LocationSales ('Alki', 3, 24, 2.6));
 
-//AllStores.push(pikePlace, seaTacAirport, southcenter, bellevue, alki);
-allStores.push(this);
+putHoursInTable();
 for(i = 0; i < allStores.length; i++){
   putSalesInTable(allStores[i]);
 }
